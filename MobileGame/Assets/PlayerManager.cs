2b6ad0cef3970b6aebe52a30e1c367711ee0c196
Arindam Pani <arindampani@gmail.com>
2019-11-07 03:52:56 -0700

    added code for ray cast , checking taps on the cage and increasing bird count once freed

---
+++ MobileGame/Assets/PlayerManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public int birdsCollected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        birdsCollected = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- MobileGame/Assets/Scripts/TapCheck.cs
+++ MobileGame/Assets/Scripts/TapCheck.cs
@@ -4,16 +4,45 @@ using UnityEngine;
 
 public class TapCheck : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
+    public GameObject cagedBirdObject;
+
+    private int lives = 2;
+    private int raycastDistance = 10;
+    private GameObject playerGameObject;
+
     void Start()
     {
-        
+        playerGameObject = GameObject.Find("Player");
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
+            Debug.DrawLine(Vector3.zero,touchPosition,Color.red);
+            //check for touch input
+            if ((touchPosition.x > transform.position.x - 1.3 && touchPosition.x < transform.position.x + 1.3) && (touchPosition.y > transform.position.y - 1.3 && touchPosition.y < transform.position.y + 1.3))
+            {
+                // Cast a ray straight on all four directions
+                RaycastHit2D hitDown = Physics2D.Raycast(transform.position, Vector2.down, raycastDistance);
+                RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, Vector2.left, raycastDistance);
+                RaycastHit2D hitRight = Physics2D.Raycast(transform.position, Vector2.right, raycastDistance);
+                RaycastHit2D hitUp = Physics2D.Raycast(transform.position, Vector2.up, raycastDistance);
+
+                // If it hits player while touching...
+                if ((hitDown.collider != null && hitDown.collider.gameObject.tag == "Player") || (hitUp.collider != null && hitUp.collider.gameObject.tag == "Player") ||
+                    (hitLeft.collider != null && hitLeft.collider.gameObject.tag == "Player") || (hitRight.collider != null && hitRight.collider.gameObject.tag == "Player"))
+                {
+                    lives--;
+                }
+
+            }
+            if (lives == 0)
+            {
+                playerGameObject.GetComponent<PlayerManager>().birdsCollected++;
+                Destroy(gameObject);
+                Destroy(cagedBirdObject);
+            }
+        }
     }
 }
