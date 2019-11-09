3ab838721ce85926064b8c8bbdaac47cfa365541
Rajesh <chilaganirajesh95@gmail.com>
2019-11-09 14:20:59 -0700

    Enemy & UI

---
+++ MobileGame/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D enemyRigidBody;
+    Vector2 maxTopPosition, maxBottomPosition;
+    bool movingTop;
+    bool movingBottom;
+    void Start()
+    {
+        enemyRigidBody = GetComponent<Rigidbody2D>();
+        maxTopPosition = enemyRigidBody.position + new Vector2(0, 2);
+        maxBottomPosition = enemyRigidBody.position + new Vector2(0, -2);
+        movingTop = true;
+        movingBottom = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movingTop)
+        {
+            enemyRigidBody.position = Vector2.MoveTowards(enemyRigidBody.position, maxTopPosition, Time.deltaTime * 2);
+        }
+        else if (movingBottom)
+        {
+            enemyRigidBody.position = Vector2.MoveTowards(enemyRigidBody.position, maxBottomPosition, Time.deltaTime * 2);
+        }
+        if (enemyRigidBody.position.y >= maxTopPosition.y)
+        {
+            movingTop = false;
+            movingBottom = true;
+        }
+        else if (enemyRigidBody.position.y <= maxBottomPosition.y)
+        {
+            movingTop = true;
+            movingBottom = false;
+        }
+
+    }
+}
---
+++ MobileGame/Assets/Scripts/EnemyRange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRange : MonoBehaviour
+{
+    public Canvas canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+           
+            collision.gameObject.transform.GetChild(0).parent = null;
+            canvas.enabled = true;
+            Destroy(collision.gameObject);
+        }
+    }
+ 
+}
