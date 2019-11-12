e6159053b17f4a0b77fa78fd9c7ed28df8e9d0ce
Rajesh <chilaganirajesh95@gmail.com>
2019-11-12 13:23:57 -0700

    Enemy & birds

--- MobileGame/Assets/Scripts/EnemyController.cs
+++ MobileGame/Assets/Scripts/EnemyController.cs
@@ -6,50 +6,27 @@ public class EnemyController : MonoBehaviour
 {
     // Start is called before the first frame update
     Rigidbody2D enemyRigidBody;
-    Vector2 maxTopPosition, maxBottomPosition;
-    bool movingTop;
-    bool movingBottom;
-    public Vector2 dynamic;
-    public bool movetop;
+    Vector2 dynamic= new Vector2(0,-1f);
+   
     void Start()
     {
-        enemyRigidBody = GetComponent<Rigidbody2D>();
-        maxTopPosition = enemyRigidBody.position +dynamic;
-        maxBottomPosition = enemyRigidBody.position - dynamic;
-        if(movetop)
-        {
-            movingTop = true;
-            movingBottom = false;
-        }
-        else
-        {
-            movingTop = false;
-            movingBottom = true;
-        }
-      
+        enemyRigidBody = GetComponent<Rigidbody2D>();      
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (movingTop)
+        enemyRigidBody.position += dynamic*2*Time.deltaTime;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.transform.position.y> enemyRigidBody.position.y)
         {
-            enemyRigidBody.position = Vector2.MoveTowards(enemyRigidBody.position, maxTopPosition, Time.deltaTime * 2);
+            dynamic.y = -1f;
         }
-        else if (movingBottom)
+        else if(collision.collider.transform.position.y < enemyRigidBody.position.y)
         {
-            enemyRigidBody.position = Vector2.MoveTowards(enemyRigidBody.position, maxBottomPosition, Time.deltaTime * 2);
+            dynamic.y = 1f;
         }
-        if (enemyRigidBody.position.y >= maxTopPosition.y)
-        {
-            movingTop = false;
-            movingBottom = true;
-        }
-        else if (enemyRigidBody.position.y <= maxBottomPosition.y)
-        {
-            movingTop = true;
-            movingBottom = false;
-        }
-
     }
 }
