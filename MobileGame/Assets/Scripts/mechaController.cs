98720b3a728c7eb54d8a18336a09a80c07d8dc0e
Rajesh <chilaganirajesh95@gmail.com>
2019-11-13 21:11:02 -0700

    cable animations

--- MobileGame/Assets/Scripts/mechaController.cs
+++ MobileGame/Assets/Scripts/mechaController.cs
@@ -72,6 +72,7 @@ public class mechaController : MonoBehaviour
                 }
                
                 gameObject.transform.GetChild(0).gameObject.SetActive(true);
+                gameObject.transform.GetChild(1).gameObject.SetActive(true);
             }
         }
         else if (gameObject.tag.Equals("FinalGate"))
@@ -115,6 +116,7 @@ public class mechaController : MonoBehaviour
                
                 Debug.Log("start blowing steam");
                 gameObject.transform.GetChild(0).gameObject.SetActive(false);
+                gameObject.transform.GetChild(1).gameObject.SetActive(false);
             }
         }
         else if (gameObject.tag.Equals("FinalGate"))
